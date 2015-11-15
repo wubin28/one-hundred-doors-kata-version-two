613cda9c9bbee518365e4b0d881751f6c7447b4f
elephant_7A8CD7C2AB <elephant@cyber-dojo.org>
2015-11-15 11:51:47 +0000

    37

--- sandbox/OneHundredDoors.java
+++ sandbox/OneHundredDoors.java
@@ -32,17 +32,6 @@ public class OneHundredDoors {
             }
         }
 
-        if (amountOfDoorsPassed == 1) {
-            doorStates.set(1, DS.OPEN);
-        } else if (amountOfDoorsPassed == 2) {
-            doorStates.set(1, DS.OPEN);
-            doorStates.set(2, DS.C);
-        } else {
-            doorStates.set(1, DS.OPEN);
-            doorStates.set(2, DS.C);
-            doorStates.set(3, DS.C);
-        }
-
         result = doorStates.subList(1, amountOfDoorsPassed + 1);
         return result.toArray(new DS[amountOfDoorsPassed]);
     }

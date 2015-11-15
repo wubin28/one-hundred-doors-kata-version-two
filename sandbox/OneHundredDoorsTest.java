bd0e92cc221cd73e1f49a5e23963cd57ebca99ff
elephant_7A8CD7C2AB <elephant@cyber-dojo.org>
2015-11-15 10:16:19 +0000

    6

--- sandbox/OneHundredDoorsTest.java
+++ sandbox/OneHundredDoorsTest.java
@@ -9,4 +9,10 @@ public class OneHundredDoorsTest {
         int actual = OneHundredDoors.answer();
         assertEquals(expected, actual);
     }
+
+    // TODO: if_there_is_only_1_door_to_pass_the_state_of_door_should_be_OPEN
+    // TODO: if_there_are_2_doors_to_pass_the_state_of_doors_should_be_OPEN_CLOSED
+    // TODO: if_there_are_3_doors_to_pass_the_state_of_doors_should_be_OPEN_CLOSED_CLOSED
+    // TODO: if_there_are_100_doors_to_pass_the_state_of_doors_should_be_all_right
+
 }

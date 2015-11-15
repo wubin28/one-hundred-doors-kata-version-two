90c364b2151e64e0e4f28916296989e3452e6365
elephant_7A8CD7C2AB <elephant@cyber-dojo.org>
2015-11-15 10:22:56 +0000

    7

--- sandbox/OneHundredDoorsTest.java
+++ sandbox/OneHundredDoorsTest.java
@@ -4,13 +4,13 @@ import static org.junit.Assert.*;
 public class OneHundredDoorsTest {
 
     @Test
-    public void life_the_universe_and_everything() {
-        int expected = 42;
-        int actual = OneHundredDoors.answer();
-        assertEquals(expected, actual);
+    public void if_there_is_only_1_door_to_pass_the_state_of_door_should_be_OPEN() {
+        // Given
+        // When
+        // Then
+        assertEquals(new D[]{D.OPEN}, oneHundredDoors.doorStatesAfterPassingDoors(1));
     }
 
-    // TODO: if_there_is_only_1_door_to_pass_the_state_of_door_should_be_OPEN
     // TODO: if_there_are_2_doors_to_pass_the_state_of_doors_should_be_OPEN_CLOSED
     // TODO: if_there_are_3_doors_to_pass_the_state_of_doors_should_be_OPEN_CLOSED_CLOSED
     // TODO: if_there_are_100_doors_to_pass_the_state_of_doors_should_be_all_right
